43718c067f9268416813c57946081483dda51fb6
leo <superleo.peking@gmail.com>
2025-04-17 16:50:35 +0800

    fix script issue

--- container-template/start.sh
+++ container-template/start.sh
@@ -75,7 +75,8 @@ export_env_vars() {
 start_jupyter() {
     if [[ $JUPYTER_PASSWORD ]]; then
         echo "Starting Jupyter Lab..."
-        mkdir -p /workspace && sudo chown ubuntu:ubuntu /workspace \
+        mkdir -p /workspace && \
+        sudo chown ubuntu:ubuntu /workspace && \
         cd / && \
         nohup python3.11 -m jupyter lab --allow-root --no-browser --port=8888 --ip=* --FileContentsManager.delete_to_trash=False --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace &> /workspace/jupyter.log &
         echo "Jupyter Lab started"
